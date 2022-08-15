07e6fef8d54062aeb28223140677ca59a46af460
edisonRam <86266391+edisonRam@users.noreply.github.com>
2022-08-15 16:34:32 -0500

    menu_estudiante y administrador actualizado

--- View/includes/menu_estudiante.php
+++ View/includes/menu_estudiante.php
@@ -19,27 +19,29 @@
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../../assets/Styles/estilo.css" />
-    <link rel=""
+    <link rel="">
 </head>
+
+
 
 <body id="page-top">
     <div id="wrapper">
         <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: black;">
 
-            <!-- Sidebar - Brand -->
+            <!-- BARRA LATERAL DE NAVEGACIÓN -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
                 <div class="sidebar-brand-icon">
                     <i class="fas fa-building"></i>
                 </div><img src="../resources/img/epis1.png" width="50" height="50">
                 <div class="sidebar-brand-text mx-3">BIBLIOTECA EPIS</div>
             </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-2">
-            <!-- S  -->
+            
+            <hr class="sidebar-divider my-2"> <!-- LINEA DIVISOR DE LOS CADA UNO DE LOS NAVEGADORES -->
+            
+            <!-- NAVEGADOR DEL ESTUDIANTE (CATALOGO, BOLETAS, CALIFICAR) -->
             <li class="nav-item">
 
-                <a href="catalogo.php" class="nav-link"><img src="../resources/img/catalogo.png" width="20" height="20">
+                <a href="catalogo_estudiante.php" class="nav-link"><img src="../resources/img/catalogo.png" width="20" height="20">
                     CATALOGO </a>
                 <a href="visualizar_boleta.php" class="nav-link"><img src="../resources/img/boleta.png" width="20"
                         height="20"> BOLETA</a>
@@ -49,12 +51,8 @@
                     </svg> CALIFICAR
                 </a>
             </li>
-
-
-
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+            
+            <hr class="sidebar-divider d-none d-md-block">  <!-- LINEA DIVISOR DE LOS CADA UNO DE LOS NAVEGADORES -->
 
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
@@ -65,17 +63,15 @@
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
 
-                <!-- Topbar -->
+                <!-- BARRA SUPERIOR PARA MOSTRAR EL NOMBRE DE USUARIO -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
 
-                    <!-- Topbar Navbar -->
+                    <!-- MOSTRAR NOMBRE DEL USUARIO LOGUEADO -->
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item text-dark ">
 
                             <?php
-                            
-                            //print_r($_SESSION['user']);
                             echo $_SESSION['user']['nombre']." ".$_SESSION['user']['apellidos']." - ";
                             echo $_SESSION['user']['tipo_usuario'];
                                                        
@@ -87,6 +83,7 @@
                         <div class="topbar-divider d-none d-sm-block">
 
                         </div>
+                        
                         <!-- Nav Item - User Information -->
 
                         <li class="nav-item dropdown no-arrow">

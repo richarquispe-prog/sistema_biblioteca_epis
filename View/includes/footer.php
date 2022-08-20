8f914720dec677e7ca64026617d5170caaeb82d6
edisonRam <86266391+edisonRam@users.noreply.github.com>
2022-08-19 22:29:07 -0500

    footer update 222

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -3,14 +3,34 @@
         <strong>EQUIPO DE DESARROLLO</strong><br>
 
         <div class="d-flex align-items-center justify-content-between small">
-            <center><div class="text-muted"> &copy; Equipo de Desarrollo: @PRODUCT OWNER: Mg. Ing. Gilmer Matos Vila | Lic. Gloria Luz Montes Bobadilla @SCRUM MASTER: Quispe Quincho Richar
-                DEVELOPMENT TEAM: @Ramos Ñahui Juan Edison @Sanchez Navarro Micky @Villazana Huaman Dante Colins 
-                    </div>
-            </center>
-            <div>
-                <!--<a href="#">Privacy Policy</a>
-                &middot;-->
-                <!--<a href="#">Terms &amp; Conditions</a>-->
+
+            <div></div>
+            <div class="text-muted">
+                <strong>SCRUM MASTER</strong>
+                <li>Mg. Ing. Gilmer Matos Vila</li>
+
+            </div>
+            <div class="text-muted">
+
+                <strong>PRODUCT OWNER</strong>
+
+                <li>Mg. Ing. Gilmer Matos Vila</li>
+                <li>Lic. Gloria Luz Montes Bobadilla</li>
+
+            </div>
+            <div class="text-muted">
+                <strong>SCRUM TEAM 2021 </strong>
+                <li>Herrera Paucar Yelina Lanasca Guillermo</li>
+                <li>Karem Briceño Carhuancho</li>
+                <li>Alexander Huamán Campos, Roberth</li>
+                <li>Chamorro Marmanillo, Javier</li>
+            </div>
+            <div class="text-muted">
+                <strong>SCRUM TEAM 2022:</strong>
+                <li>Ramos Ñahui Juan Edison</li>
+                <li>Sanchez Navarro Micky</li>
+                <li>Villazana Huaman Dante Colins</li>
+                <li>Quispe Quincho Richar</li>
             </div>
             <div></div>
         </div>

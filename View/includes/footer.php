6390fb06b19aefb73e71db5fc95c298b1d228a9d
richarquispe-prog <richarelrey4@gmail.com>
2022-08-19 19:37:20 -0500

    actualizado

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,14 +1,11 @@
 <footer class="footer py-4 bg-light mt-auto">
     <div class="container-fluid">
         <div class="d-flex align-items-center justify-content-between small">
-            <center><div class="text-muted"> &copy; Equipo de Desarrollo: @PRODUCT OWNER: Mg. Ing. Gilmer Matos Vila | Lic. Gloria Luz Montes Bobadilla @SCRUM MASTER: Quispe Quincho Richar
-                DEVELOPMENT TEAM: @Ramos Ñahui Juan Edison @Sanchez Navarro Micky @Villazana Huaman Dante Colins 
+            <center><div class="text-muted"> &copy; Equipo de Desarrollo: @PRODUCT OWNER: Mg. Ing. Gilmer S. Matos Vila | Lic. Gloria Luz Montes Bobadilla @SCRUM MASTER: Mg. Ing. Gilmer S. Matos Vila
+                DEVELOPMENT TEAM: @Quispe Quincho Richar @Ramos Ñahui Juan Edison @Sanchez Navarro Micky @Villazana Huaman Dante Colins 
                     </div>
             </center>
             <div>
-                <!--<a href="#">Privacy Policy</a>
-                &middot;-->
-                <!--<a href="#">Terms &amp; Conditions</a>-->
             </div>
         </div>
     </div>
